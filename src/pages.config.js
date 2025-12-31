80b6be01cf8980bbe5296a402b210c01bc63c307
Aviel Cohen <coaviel@gmail.com>
2025-12-31 17:18:23 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,22 +1,16 @@
 import CookiePolicy from './pages/CookiePolicy';
-import Cookies from './pages/Cookies';
 import Home from './pages/Home';
-import Privacy from './pages/Privacy';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import PublicReport from './pages/PublicReport';
-import Terms from './pages/Terms';
 import TermsOfService from './pages/TermsOfService';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
     "CookiePolicy": CookiePolicy,
-    "Cookies": Cookies,
     "Home": Home,
-    "Privacy": Privacy,
     "PrivacyPolicy": PrivacyPolicy,
     "PublicReport": PublicReport,
-    "Terms": Terms,
     "TermsOfService": TermsOfService,
 }
 
