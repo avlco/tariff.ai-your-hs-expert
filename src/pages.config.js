fe2ba066b34e53796cd128e9f04bae052b769b01
Aviel Cohen <aviel.cohen@gramss.app>
2026-01-02 09:26:24 +0200

    Refactor pages configuration and imports
    Refactored pages configuration to include routes and updated import syntax.

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,21 +1,27 @@
-import CookiePolicy from './pages/CookiePolicy';
-import Home from './pages/Home';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import PublicReport from './pages/PublicReport';
-import TermsOfService from './pages/TermsOfService';
-import __Layout from './Layout.jsx';
-
-
-export const PAGES = {
-    "CookiePolicy": CookiePolicy,
-    "Home": Home,
-    "PrivacyPolicy": PrivacyPolicy,
-    "PublicReport": PublicReport,
-    "TermsOfService": TermsOfService,
-}
+import Home from "./pages/Home";
+import TermsOfService from "./pages/TermsOfService";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import CookiePolicy from "./pages/CookiePolicy";
+import PublicReport from "./pages/PublicReport";
+import Layout from "./Layout";
 
 export const pagesConfig = {
-    mainPage: "Home",
-    Pages: PAGES,
-    Layout: __Layout,
+  mainPage: 'Home',
+  visitedPage: 'Home',
+  pages: {
+    'Home': Home,
+    'Terms': TermsOfService,
+    'Privacy': PrivacyPolicy,
+    'Cookies': CookiePolicy,
+    'PublicReport': PublicReport
+  },
+  // מיפוי נתיבים לקומפוננטות
+  routes: [
+    { path: '/', component: Home },
+    { path: '/terms', component: TermsOfService },
+    { path: '/privacy', component: PrivacyPolicy },
+    { path: '/cookies', component: CookiePolicy },
+    { path: '/report', component: PublicReport },
+  ],
+  Layout: Layout 
 };
